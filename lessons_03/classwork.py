9db64f5e3d995fca97694a6f9538930a93c38ab0
Alexander Vasyuk <ssh74vsk@gmail.com>
2022-05-20 21:43:53 +0300

    classwork

---
+++ lessons_03/classwork.py
@@ -0,0 +1,33 @@
+x1 = 17 / 2 * 3 + 2
+x2 = 2 + 17 / 2 * 3
+x3 = 19 % 4 + 15 / 2 * 3
+x4 = (15 + 6) - 10 * 4
+x5= 17 / 2 % 2 * 3
+print(x1, x2, x3, x4, x5)
+
+
+x1 = 17 / 2 * (3 + 2)
+x2 = (2 + 17) / 2 * 3
+x3 = 19 % (4 + 15) / 2 * 3
+x4 = ((15 + 6) - 10) * 4
+x5= 17 / 2 % (2 * 3)
+print(x1, x2, x3, x4, x5)
+
+
+ivan, petr, igor, = 23, 38, 23
+print(ivan + petr + igor)
+print(ivan + petr + igor) / 3)
+
+
+my_list = [1, 1.0, 2, 2, 5.0, "python", "python3", "python3"]
+my_set = set(my_list)
+print(len(my_list) - len(my_set))
+
+
+my_slice = my_list[3:6]
+print(my_slice[::-1])
+
+
+size = 5
+print([size * 4, size ** 2, size * size, (size ** 2 * 2) ** 0.5])
+
