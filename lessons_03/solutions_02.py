a2d371b9814a2b08b510ab9e6fe101cdb58772ee
Alexander Vasyuk <ssh74vsk@gmail.com>
2022-05-22 22:04:15 +0300

    homework_2

---
+++ lessons_03/solutions_02.py
@@ -0,0 +1,7 @@
+year1 = (2130 * 0.1) + 2310 + 120
+year2 = (year1 * 0.1) + year1 + 120
+year3 = (year2 * 0.1) + year2 + 120
+year4 = (year3 * 0.1) + year3 + 120
+year5 = (year4 * 0.1) + year4 + 120
+print("%.2f" % year5)
+
